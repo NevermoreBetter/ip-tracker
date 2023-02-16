03e3da6f904ddb50bfb31282be718825c4ccb1dd
NevermoreBetter <72225613+NevermoreBetter@users.noreply.github.com>
2023-02-16 19:02:58 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
 import "./index.css";
 
 function App() {
@@ -45,14 +46,9 @@ function App() {
     e.preventDefault();
     setIpInput(ipAddress);
   }
-  console.log(
-    "2023-02-16T14:24:48+01:00".slice(
-      "2023-02-16T14:24:48+01:00".length - 6,
-      "2023-02-16T14:24:48+01:00".length
-    )
-  );
+
   useEffect(() => {
-    getLocation();
+    // getLocation();
   }, [IpInput]);
 
   function ChangeView({ center }) {
@@ -60,6 +56,13 @@ function App() {
     map.setView(center);
     return null;
   }
+
+  const svgIcon = L.divIcon({
+    html: `<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>`,
+    className: "svg-icon",
+    iconSize: [24, 40],
+    iconAnchor: [22, 40],
+  });
 
   return (
     <div>
@@ -114,9 +117,7 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={center}>
-            <Popup>Ip Location</Popup>
-          </Marker>
+          <Marker position={center} icon={svgIcon}></Marker>
         </MapContainer>
       </div>
     </div>
